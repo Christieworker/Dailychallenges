c7126b3131379cfb6217f57c846caeab39a17dbb
jcaitano <jenycaitano@gmail.com>
2023-01-24 09:42:22 +0000

    monsupertriangle2

--- monsupertriangle.go
+++ monsupertriangle.go
@@ -7,16 +7,32 @@ func superTriangle(carac string, nblines int) {
 	fmt.Scanln(&carac)
 	fmt.Println(carac)
 
-	if /*len(carac) > 1 ||*/ carac == " " {
+	/*for index, rune := range carac {
+		fmt.Printf("%c starts at byte %d\n", rune, index)
+	}*/
+
+	fmt.Println("Combien de lignes")
+	fmt.Scanln(&nblines)
+	fmt.Println(nblines)
+
+	if carac == " " {
 		fmt.Println("Saisissez un seul caractère et un caractère différent du caractère espace")
 		fmt.Scanln(&carac)
 		fmt.Println(carac)
 	}
 
+	/*if len(carac) > 1 || carac == " " {
+		fmt.Println("Saisissez un seul caractère et un caractère différent du caractère espace")
+		fmt.Scanln(&carac)
+		fmt.Println(carac)
+	}*/
+
 	if nblines > 16 {
 		fmt.Println("Vous ne devez pas dépasser 16 lignes. Entrez à nouveau le nombre de ligne")
+		fmt.Scanln(&nblines)
 		fmt.Println(nblines)
 	}
+
 }
 
 func main() {
